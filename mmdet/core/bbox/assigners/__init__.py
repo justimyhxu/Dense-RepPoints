04a3f4cc98754142f6a851d7f2bd8275b26130d2
Yinghao Xu <justimyhxu@zju.edu.cn>
2020-03-30 01:08:54 +0800

    add assigner

--- mmdet/core/bbox/assigners/__init__.py
+++ mmdet/core/bbox/assigners/__init__.py
@@ -4,8 +4,9 @@ from .atss_assigner import ATSSAssigner
 from .base_assigner import BaseAssigner
 from .max_iou_assigner import MaxIoUAssigner
 from .point_assigner import PointAssigner
+from .point_box_assigner import PointBoxAssigner
 
 __all__ = [
     'BaseAssigner', 'MaxIoUAssigner', 'ApproxMaxIoUAssigner', 'AssignResult',
-    'PointAssigner', 'ATSSAssigner'
+    'PointAssigner', 'ATSSAssigner', 'PointBoxAssigner'
 ]
